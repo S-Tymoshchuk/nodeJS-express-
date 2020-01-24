bf456bf1f08f23230ef085c2f1419bea17e60f3e
Sergey <lobalmax@gmail.com>
2020-01-24 13:44:11 +0200

    Динамические параметры

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const hbs = exphbs.create({
     extname: "hbs"
 });
 
+
+
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
--- routes/courses.js
+++ routes/courses.js
@@ -12,4 +12,13 @@ router.get("/", async (req, res) => {
     });
 });
 
+router.get("/:id", async (req, res) => {
+    const course = await Course.getById(req.params.id);
+    res.render("courseHbs", {
+        layout: "emty",
+        title: `Курс ${course.title}`,
+        course
+    });
+});
+
 module.exports = router;
